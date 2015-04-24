285bba129f1c9f82484cb3a2230ec1a42e31965c
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-04-23 21:43:46 -0700

    shuffle array algorithm implementation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,21 @@
  */
 angular.module('mindgamesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.scores = [1, 2];
+    $scope.scores = [5, 6, 3, 9];
+
+    function shuffleArray(arr) {
+    	arr = $scope.scores;
+    	var len = arr.length;
+
+    	for(var i=len-1; i>=0; i--) {
+    		var randNum = Math.floor((Math.random() * i) + 1);
+    		var temp = arr[randNum];
+    		arr[randNum] = arr[i];
+    		arr[i] = temp;
+    	}
+
+    	console.log(arr);
+    }
+
+   	shuffleArray();
   });
