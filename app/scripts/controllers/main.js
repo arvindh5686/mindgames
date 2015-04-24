8349cdbf2632a3f5eb7406c57b3bbde2a7197d00
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-04-23 20:48:20 -0700

    create templates for rendering images in grid

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('mindgamesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.scores = [1, 2];
   });
