65df1ca93bd376e328335b93babfac6343aa7596
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-04-26 19:02:04 -0700

    implement dynamic loading of images

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,26 @@
  */
 angular.module('mindgamesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.scores = [5, 6, 3, 9];
+    $scope.max = 1;
+    $scope.master = [];
+    $scope.scores = [0, 1, 2, 3];
+    $scope.shuffledArray = [];
+
+    var map = {
+    	'0' : 'batman.gif',
+    	'1' : 'courage.gif',
+    	'2' : 'dexter.gif',
+    	'3' : 'calvinhobbes.jpeg'
+    };
+
+    $scope.range = function(min, max, step){
+	    step = step || 1;
+	    var input = [];
+	    for (var i = min; i <= max; i += step) {
+	    	input.push(i);
+	    }
+	    return input;
+  	};
 
     function shuffleArray(arr) {
     	arr = $scope.scores;
@@ -22,7 +41,23 @@ angular.module('mindgamesApp')
     		arr[i] = temp;
     	}
 
+    	$scope.shuffledArray = arr;
     	console.log(arr);
+    	create2DArray();
+    }
+
+    
+    function create2DArray() {
+    	var count = 0;
+    	for(var i=0;i<=$scope.max;i++) {
+    		var arr = [];
+    		for(var j=0;j<=$scope.max;j++) {
+    			arr.push(map[$scope.shuffledArray[count++]]);
+    		}
+    		$scope.master.push(arr);
+    	}
+
+    	console.log($scope.master);
     }
 
    	shuffleArray();
