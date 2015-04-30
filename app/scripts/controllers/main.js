2128d197f87706283612bd043b09b1abc03479c0
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-04-30 00:00:51 -0700

    add flip on hover and added target image to match

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,14 +12,20 @@ angular.module('mindgamesApp')
     $scope.max = 1;
     $scope.master = [];
     $scope.scores = [0, 1, 2, 3];
-    $scope.shuffledArray = [];
-
+    var masterIndex = 0
+    
     var map = {
     	'0' : 'batman.gif',
     	'1' : 'courage.gif',
     	'2' : 'dexter.gif',
     	'3' : 'calvinhobbes.jpeg'
     };
+
+    $scope.clickme = function() {
+    	//alert("1");
+    	var containersArr = shuffleArray($('.main_container .main_card'));
+    	flipImages(containersArr, 0);
+    }
 
     $scope.range = function(min, max, step){
 	    step = step || 1;
@@ -31,7 +37,6 @@ angular.module('mindgamesApp')
   	};
 
     function shuffleArray(arr) {
-    	arr = $scope.scores;
     	var len = arr.length;
 
     	for(var i=len-1; i>=0; i--) {
@@ -41,18 +46,18 @@ angular.module('mindgamesApp')
     		arr[i] = temp;
     	}
 
-    	$scope.shuffledArray = arr;
     	console.log(arr);
-    	create2DArray();
+    	return arr;
     }
 
     
-    function create2DArray() {
+    function create2DArray(shuffledArray) {
+    	console.log(shuffledArray);
     	var count = 0;
     	for(var i=0;i<=$scope.max;i++) {
     		var arr = [];
     		for(var j=0;j<=$scope.max;j++) {
-    			arr.push(map[$scope.shuffledArray[count++]]);
+    			arr.push(map[shuffledArray[count++]]);
     		}
     		$scope.master.push(arr);
     	}
@@ -60,5 +65,40 @@ angular.module('mindgamesApp')
     	console.log($scope.master);
     }
 
-   	shuffleArray();
+    function flipImages(containersArr, index) {
+    	if(index === arr.length) {
+    		addClickEvent();
+    		renderDraggableImage();
+    		return;
+    	}
+    	$(containersArr[index]).css('transform', 'rotateY(180deg)');
+    	setTimeout(function() {
+    		$(containersArr[index]).css('transform', 'rotateY(0deg)');
+    		flipImages(containersArr, index + 1);
+    	}, 1000)
+    }
+
+    function addClickEvent() {
+    	var containersArr = shuffleArray($('.main_container .main_card'));
+    	for(var i=0;i<containersArr.length;i++) {
+    		$(containersArr[i]).click(function() {
+    			var currName = $('#dragDropImg').find('img').attr("name");
+    			var selectedName = $(this).attr("name");
+    			if(currName == selectedName) {
+    				alert("success");
+    				masterIndex++;
+    				renderDraggableImage();
+    			}
+    			else alert("failure");
+    		})
+    	}
+    }
+
+    function renderDraggableImage() {
+    	$('#dragDropImg').find('img').attr("src","images/" + map[masterIndex]);
+    	$('#dragDropImg').find('img').attr("name", map[masterIndex])
+    }
+
+   	var arr = shuffleArray($scope.scores);
+   	create2DArray(arr);
   });
