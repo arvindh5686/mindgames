102b14d484a4b3b4679d52f9403f35dd2dbaf939
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-05-06 21:23:27 -0700

    add server

---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Dependencies
+var server = require( './../server' ).server;
+
+// Router
+// Index route - usually served as static via express
+server.get( '/', require( './main' ) );
+
+// Example route
+server.get( '/route/:route', require( './example' ) );
+
+// Catch all
+server.get( '*', require( './main' ) );
+
+
---
+++ server/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+var express     = require( 'express' ),
+    http        = require( 'http' ),
+    path        = require( 'path' );
+
+// Server instance
+var server = exports.server = express();
+
+// Configure Server
+server.configure( function() {
+    server.set( 'port', 9000 );
+    server.set( 'views', path.join( __dirname, './../app' ) );
+    
+    server.use( express.bodyParser() );
+    server.use( express.methodOverride() );
+    server.use( express.static( path.join( __dirname, './../app' ) ) );
+    server.use( server.router );
+} );
+
+// Start server 
+http.createServer( server ).listen( server.get( 'port' ), function() {
+    console.log( 'Express server listening on ' + server.get( 'port' ) );
+} );
+
+// Configure Routes
+require( './routes' );
