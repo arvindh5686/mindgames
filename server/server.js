ea5fb7a11fe66e2eccd07bd503328e67c3312c39
Aravindh Balakrishnan <abalakrishnan0@walmartlabs.com>
2016-10-23 11:49:57 -0700

    add npm strt

--- server/server.js
+++ server/server.js
@@ -14,15 +14,16 @@ var server = exports.server = express();
 // Configure Server
 server.configure( function() {
     server.set( 'port', 9000 );
+    console.log(path.join( __dirname, './../app' ));
     server.set( 'views', path.join( __dirname, './../app' ) );
-    
+
     server.use( express.bodyParser() );
     server.use( express.methodOverride() );
     server.use( express.static( path.join( __dirname, './../app' ) ) );
     server.use( server.router );
 } );
 
-// Start server 
+// Start server
 http.createServer( server ).listen( server.get( 'port' ), function() {
     console.log( 'Express server listening on ' + server.get( 'port' ) );
 } );
